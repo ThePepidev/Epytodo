77b114f38084c385a020a505c48c0282a83d8a4c
Mathys <pepidev@epitech.eu>
2025-05-19 16:21:17 +0200

    fix(user.query.js): fixing date format print issue

--- src/routes/user/user.query.js
+++ src/routes/user/user.query.js
@@ -2,36 +2,43 @@ const db = require('../../config/db');
 
 exports.getUserById = (id, res) => {
     if (!id)
-      return res.status(400).json({ error: 'missing id' });
+      return res.status(400).json({ msg: 'missing id' });
     db.query('SELECT id, email, name, firstname, created_at FROM user WHERE id = ?', [id], (err, results) => {
     if (err)
-      return res.status(500).json({ error: 'DB error' });
+      return res.status(500).json({ msg: 'DB error' });
     if (results.length === 0)
-      return res.status(404).json({ error: 'User not found' });
-    res.json(results[0]);
+      return res.status(404).json({ msg: 'User not found' });
+    const user = results[0];
+    user.created_at = new Date(user.created_at).toISOString().replace('T', ' ').replace('Z', '');
+    res.json(user);
   });
 };
 
 exports.getUserTodos = (id, res) => {
     if (!id)
-      return res.status(400).json({ error: 'missing id' });
+      return res.status(400).json({ msg: 'missing id' });
     db.query('SELECT * FROM todo WHERE user_id = ?', [id], (err, results) => {
     if (err)
-      return res.status(500).json({ error: 'DB error' });
+      return res.status(500).json({ msg: 'DB error' });
     if (results.length === 0)
       return res.status(404).json({ msg: 'No todos found for this user' });
-    res.json(results);
+      const todo = results;
+    for (let i = 0; i < todo.length; i++) {
+      todo[i].created_at = new Date(todo[i].created_at).toISOString().replace('T', ' ').replace('Z', '');
+      todo[i].due_time = new Date(todo[i].due_time).toISOString().replace('T', ' ').replace('Z', '');
+    }
+    return res.status(200).json(todo);
   });
 };
 
 exports.getUserByEmail = (email, res) => {
     if (!email)
-      return res.status(400).json({ error: 'missing email' });
+      return res.status(400).json({ msg: 'missing email' });
     db.query('SELECT id, email, name, firstname, created_at FROM user WHERE email = ?', [email], (err, results) => {
     if (err)
-      return res.status(500).json({ error: 'DB error' });
+      return res.status(500).json({ msg: 'DB error' });
     if (results.length === 0)
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ msg: 'User not found' });
     res.json(results[0]);
   });
 };
